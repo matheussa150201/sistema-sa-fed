9ca54a274b122b39e85c6909bb2cc13b7c5c44bc
victordanielrn <danielrn14@hotmail.com>
2023-02-08 17:29:37 -0300

    FrontEND

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerLoginComponent } from './containers/container-login/container-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '',
+  redirectTo: 'login',
+  pathMatch:'full'
+},
+{
+  path:'login',
+  component: ContainerLoginComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/containers/container-login/container-login.component.ts
+++ src/app/containers/container-login/container-login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import { ContanierloginService } from 'src/app/service/contanierlogin.service';
 import {Login} from "../../models/login";
+
 
 @Component({
   selector: 'app-container-login',
@@ -8,9 +10,13 @@ import {Login} from "../../models/login";
   styleUrls: ['./container-login.component.scss']
 })
 export class ContainerLoginComponent implements OnInit {
-
+  login!: Login;
   formLogin! : FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private containerloginservice: ContanierloginService
+    ) { 
+      this.getLogin()
+    }
 
   ngOnInit() {
     this.criarFormularioLogin(new Login())
@@ -23,8 +29,12 @@ export class ContainerLoginComponent implements OnInit {
     })
   }
 
+
   public entrar(){
     window.alert('email: ' + this.formLogin?.value?.email + ' senha : ' + this.formLogin?.value?.senha)
   }
 
+  getLogin():void{
+    this.containerloginservice.getLogin().subscribe((login) => (this.login = login ));
+  }
 }
